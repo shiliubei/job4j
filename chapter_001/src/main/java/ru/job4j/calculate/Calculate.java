ec90ff907e437624ff24499b01fa9454670e659e
shiliubei <shiliubei@gmail.com>
2018-08-12 21:03:43 +0900

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,11 @@
 public class Calculate {
-    
+	/**
+	* Package for calculate task.
+	*
+	* @author Danil Liu (shiliubei@gmail.com)
+	* @version $0.1$
+	* @since 12.08.18
+	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
