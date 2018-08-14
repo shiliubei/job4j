76b8e2a0d0afc37eb064c26e6e20fe4e8ab6876a
shiliubei <shiliubei@gmail.com>
2018-08-14 20:27:31 +0900

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,33 @@
+
+
+/**
+* Package for calculate task.
+*
+* @author Danil Liu (shiliubei@gmail.com)
+* @version $0.1$
+* @since 12.08.18
+*/
+package ru.job4j.calculate;
+
 public class Calculate {
+	
+	
 	/**
-	* Package for calculate task.
-	*
-	* @author Danil Liu (shiliubei@gmail.com)
-	* @version $0.1$
-	* @since 12.08.18
-	*/
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+    }
+	
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+
+        return "Echo, echo, echo : " + name;
     }
 }
