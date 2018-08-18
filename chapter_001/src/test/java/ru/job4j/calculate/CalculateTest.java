bac8400cae933fbfe3277c71905be608754bbf91
shiliubei <shiliubei@gmail.com>
2018-08-18 18:56:37 +0900

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -8,24 +8,24 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Package for calculate task.
-*
-* @author Danil Liu (shiliubei@gmail.com)
-* @version $0.1$
-* @since 12.08.18
-*/
+ * Package for calculate task.
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 12.08.18
+ */
 
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Liu";
-    String expect = "Echo, echo, echo : Liu"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Liu";
+        String expect = "Echo, echo, echo : Liu";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
