fba259de8e6c13639ae579a7389f712192e08618
shiliubei <shiliubei@gmail.com>
2018-08-19 11:14:11 +0900

    4.1. Глупый бот. [31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+
+/**
+ * Package for calculate task.
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 12.08.18
+ */
+
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            // заменить ... на проверку, что этот вопрос известен боту и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
