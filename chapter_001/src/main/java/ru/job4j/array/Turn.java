4359a0dc89df3a2c649fd18cf12532ba7321da36
shiliubei <shiliubei@gmail.com>
2018-08-22 23:19:04 +0900

    6.2. Перевернуть массив. [#4441]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * backward array without using a buffer variable
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 22.08.18
+ */
+public class Turn {
+    /**
+     * change elements from first one up to middle element except middle element.
+     * @param  array initial array.
+     * @return array - backward array.
+     */
+    public int[] turn(int[] array) {
+        for (int i = 0; i<array.length/2; i++) {
+            array[i] = array[i]+array[array.length-1-i];
+            array[array.length-1-i] = array[i] - array[array.length-1-i];
+            array[i] = array[i] - array[array.length-1-i];
+        }
+        return array;
+    }
+}
