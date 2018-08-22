3e32abdf1ed80bc455c9dbafef6752b697213fac
shiliubei <shiliubei@gmail.com>
2018-08-22 20:22:17 +0900

    5.5. Рефакторинг кода. [#33459]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,51 @@
+package ru.job4j.loop;
+import java.util.function.BiPredicate;
+/**
+ * Paint fo refactoring.
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 22.08.18
+ */
+
+
+    public class Paint {
+        public String rightTrl(int height) {
+            return this.loopBy(
+                    height,
+                    height,
+                    (row, column) -> row >= column
+            );
+        }
+
+        public String leftTrl(int height) {
+            return this.loopBy(
+                    height,
+                    height,
+                    (row, column) -> row >= height - column - 1
+            );
+        }
+
+        public String pyramid(int height) {
+            return this.loopBy(
+                    height,
+                    2 * height - 1,
+                    (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+            );
+        }
+
+        private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+            StringBuilder screen = new StringBuilder();
+            for (int row = 0; row != height; row++) {
+                for (int column = 0; column != weight; column++) {
+                    if (predict.test(row, column)) {
+                        screen.append("^");
+                    } else {
+                        screen.append(" ");
+                    }
+                }
+                screen.append(System.lineSeparator());
+            }
+            return screen.toString();
+        }
+    }
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
