62c1d5f535b213cf939a3353c4fb62d9a901bf96
shiliubei <shiliubei@gmail.com>
2018-08-23 20:57:59 +0900

    6.1. Классический поиск перебором.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,9 +1,23 @@
 package ru.job4j.array;
 
+/**
+ * Finding an element in array.
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 22.08.18
+ */
 public class FindLoop {
+    /**
+     *  finds the required element.
+     *
+     * @param data array where we will find.
+     * @param el   element that need to find.
+     * @return rst - array index of the search element or -1 when it's has not.
+     */
     public int indexOf(int[] data, int el) {
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for (int index = 0;index < data.length-1; index ++) {
+        int rst = -1;
+        for (int index = 0; index <= data.length - 1; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,24 +1,33 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test for finding an element in array.
+ *
+ * @author Danil Liu (shiliubei@gmail.com)
+ * @version $0.1$
+ * @since 22.08.18
+ */
 public class FindLoopTest {
 
     @Test
     public void whenArrayHasLengh5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{6, 10, 5};
         int value = 5;
         int result = find.indexOf(input, value);
-        int expect = 0;
+        int expect = 2;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayThen0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {8, 7, 6};
+        int[] input = new int[]{8, 7, 6};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = -1;
